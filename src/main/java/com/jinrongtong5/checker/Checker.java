29379bf751034e843e1845cc77fdefb671244f86
rongtongjin <794220751@qq.com>
2019-10-14 17:40:53 +0800

    Update Checker.java

--- src/main/java/com/jinrongtong5/checker/Checker.java
+++ src/main/java/com/jinrongtong5/checker/Checker.java
@@ -15,20 +15,20 @@ public class Checker {
 
         Set<String> enqueueSet = getLinesStream().filter(x -> x[1].equals("enqueue")).map(x -> x[2]).collect(
             Collectors.toSet());
-        System.out.println("enqueueSet size: " + enqueueSet.size());
         Set<String> dequeueSet = getLinesStream().filter(x -> x[1].equals("dequeue")).map(x -> x[2]).collect(
             Collectors.toSet());
-        System.out.println("dequeueSet size: " + enqueueSet.size());
+        long enqueueCount = getLinesStream().filter(x -> x[1].equals("enqueue")).map(x -> x[2]).count();
+        long dequeueCount = getLinesStream().filter(x -> x[1].equals("dequeue")).map(x -> x[2]).count();
+        System.out.println("enqueue count: " + enqueueCount);
+        System.out.println("dequeue count: " + dequeueCount);
         boolean noMissingMessages = dequeueSet.containsAll(enqueueSet);
         boolean noUnexpectMessages = enqueueSet.containsAll(dequeueSet);
-
         System.out.println("No missing messages: " + noMissingMessages);
         System.out.println("No unexpect messages: " + noUnexpectMessages);
         if (noUnexpectMessages) {
-            long dequeueCount = getLinesStream().filter(x -> x[1].equals("dequeue")).map(x -> x[2]).count();
-            System.out.println("Number of duplicate messages: "+(dequeueCount-dequeueSet.size()));
+            System.out.println("Number of duplicate messages: " + (dequeueCount - dequeueSet.size()));
         }
-        System.out.println("valid: "+(noMissingMessages&noUnexpectMessages));
+        System.out.println("valid: " + (noMissingMessages & noUnexpectMessages));
     }
 
     public static Stream<String[]> getLinesStream() throws IOException {
